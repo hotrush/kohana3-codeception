3bdafefd6bb25c714b3b618ab6d72a3d11d8b37b
rush <johny64@gmail.com>
2015-09-07 12:19:08 +0300

    initial commit

---
+++ src/Codeception/Lib/Connector/Kohana3.php
@@ -0,0 +1,50 @@
+<?php
+namespace Codeception\Lib\Connector;
+
+use Symfony\Component\BrowserKit\Response;
+use Symfony\Component\BrowserKit\Client;
+
+class Kohana3 extends Client
+{
+
+    /**
+     * @param SymfonyRequest $request
+     * @return Response
+     */
+    protected function doRequest($request)
+    {
+
+        $_COOKIE = $request->getCookies();
+        $_SERVER = $request->getServer();
+        $_FILES = $request->getFiles();
+
+        $uri = str_replace('http://localhost', '', $request->getUri());
+
+        $_SERVER['KOHANA_ENV'] = 'testing';
+        $_SERVER['REQUEST_METHOD'] = strtoupper($request->getMethod());
+        $_SERVER['REQUEST_URI'] = strtoupper($uri);
+
+        $kohanaRequest = \Request::factory($uri);
+        $kohanaRequest->method($_SERVER['REQUEST_METHOD']);
+        if (strtoupper($request->getMethod()) == 'GET') {
+            $kohanaRequest->query($request->getParameters());
+        }
+        if (strtoupper($request->getMethod()) == 'POST') {
+            $kohanaRequest->post($request->getParameters());
+        }
+
+        if (isset($_SERVER['PHP_AUTH_USER']) && isset($_SERVER['PHP_AUTH_PW'])) {
+            $kohanaRequest->headers('Authorization', 'Basic '.base64_encode($_SERVER['PHP_AUTH_USER'].':'.$_SERVER['PHP_AUTH_PW']));
+        }
+        $kohanaRequest->cookie($_COOKIE);
+        $kohanaRequest::$initial = $kohanaRequest;
+        $kohanaResponse = $kohanaRequest->execute();
+        $content = $kohanaResponse->body();
+        $headers = (array) $kohanaResponse->headers();
+        $status = $kohanaResponse->status();
+        $response = new Response($content, $status, $headers);
+        return $response;
+
+    }
+
+}
---
+++ src/Codeception/Module/Kohana3.php
@@ -0,0 +1,147 @@
+<?php
+namespace Codeception\Module;
+
+use Codeception\Exception\ModuleConfigException;
+use Codeception\Lib\Connector\Kohana3 as KohanaConnector;
+use Codeception\Lib\Framework;
+use Codeception\Lib\ModuleContainer;
+
+class Kohana3 extends Framework
+{
+    /**
+     * @var array
+     */
+    public $config = [];
+
+    /**
+     * Constructor.
+     *
+     * @param ModuleContainer $container
+     * @param $config
+     */
+    public function __construct(ModuleContainer $container, $config = null)
+    {
+        $this->config = array_merge(
+            [
+                'bootstrap' => 'bootstrap.php',
+                'application_dir' => 'application',
+                'modules_dir' => 'modules',
+                'system_dir' => 'system',
+                'custom_config_reader' => null,
+            ],
+            (array)$config
+        );
+
+        $projectDir = \Codeception\Configuration::projectDir();
+
+        if (!defined('EXT')) {
+            define('EXT', '.php');
+        }
+        if (!defined('DOCROOT')) {
+            define('DOCROOT', realpath($projectDir).DIRECTORY_SEPARATOR);
+        }
+        if (!defined('APPPATH')) {
+            define('APPPATH', realpath(DOCROOT.$this->config['application_dir']).DIRECTORY_SEPARATOR);
+        }
+        if (!defined('MODPATH')) {
+            define('MODPATH', realpath(DOCROOT.$this->config['modules_dir']).DIRECTORY_SEPARATOR);
+        }
+        if (!defined('SYSPATH')) {
+            define('SYSPATH', realpath(DOCROOT.$this->config['system_dir']).DIRECTORY_SEPARATOR);
+        }
+
+        if (!defined('KOHANA_START_TIME')) {
+            define('KOHANA_START_TIME', microtime(TRUE));
+        }
+        if (!defined('KOHANA_START_MEMORY')) {
+            define('KOHANA_START_MEMORY', memory_get_usage());
+        }
+
+        if (!defined('API_MODE')) {
+            define('API_MODE', true);
+        }
+
+        $this->config['bootstrap_file'] = APPPATH . $this->config['bootstrap'];
+
+        parent::__construct($container);
+    }
+
+    /**
+     * Initialize hook.
+     */
+    public function _initialize()
+    {
+        $this->checkBootstrapFileExists();
+        $this->loadBootstrap();
+        $this->setConfigReader();
+        $this->client = new KohanaConnector();
+    }
+
+    /**
+     * Before hook.
+     *
+     * @param \Codeception\TestCase $test
+     */
+    public function _before(\Codeception\TestCase $test)
+    {
+
+    }
+
+    /**
+     * After hook.
+     *
+     * @param \Codeception\TestCase $test
+     */
+    public function _after(\Codeception\TestCase $test)
+    {
+
+    }
+
+    /**
+     * After step hook.
+     *
+     * @param \Codeception\Step $step
+     */
+    public function _afterStep(\Codeception\Step $step)
+    {
+        parent::_afterStep($step);
+    }
+
+    /**
+     * Make sure the Laravel bootstrap file exists.
+     *
+     * @throws ModuleConfigException
+     */
+    protected function checkBootstrapFileExists()
+    {
+        $bootstrapFile = $this->config['bootstrap_file'];
+
+        if (!file_exists($bootstrapFile)) {
+            throw new ModuleConfigException(
+                $this,
+                "Kohana bootstrap file not found in $bootstrapFile.\nPlease provide a valid path to it using 'bootstrap' config param. "
+            );
+        }
+    }
+
+    /**
+     * Load Kohana bootstrap
+     */
+    protected function loadBootstrap()
+    {
+        require $this->config['bootstrap_file'];
+    }
+
+    /**
+     * Load modified config loader
+     */
+    protected function setConfigReader()
+    {
+        if ($this->config['custom_config_reader'])
+        {
+            \Kohana::$config->detach(new \Config_File);
+            \Kohana::$config->attach(new $this->config['custom_config_reader']);
+        }
+    }
+
+}
